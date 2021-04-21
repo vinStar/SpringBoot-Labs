147cc719043ac87cd32194593d981db846458dde
vin <731315455@qq.com>
2021-04-21 11:51:21 +0800

    + f , 1、property descriptor 2、classLoader

--- lab-71-http-debug/lab-71-idea-http-client/src/main/java/cn/iocoder/springboot/lab71/controller/UserController.java
+++ lab-71-http-debug/lab-71-idea-http-client/src/main/java/cn/iocoder/springboot/lab71/controller/UserController.java
@@ -3,7 +3,6 @@ package cn.iocoder.springboot.lab72.controller;
 import cn.iocoder.springboot.lab72.vo.UserUpdateVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
